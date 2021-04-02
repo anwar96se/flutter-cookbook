b016db84c1b3e0a125b67d8e9d494733ef8336d3
Anwar <anwar.dev.96@gmail.com>
2021-04-02 14:12:00 +0300

    Use lists

---
+++ lib/lists/basic-list.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+final title = 'Use lists';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(fontFamily: 'Raleway'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.map),
+            title: Text('Map'),
+          ),
+          ListTile(
+            leading: Icon(Icons.photo_album),
+            title: Text('Album'),
+          ),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('Phone'),
+          ),
+        ],
+      ),
+    );
+  }
+}
